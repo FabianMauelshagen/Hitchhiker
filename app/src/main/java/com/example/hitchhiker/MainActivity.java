8fe782ddc506ccb9026c8cf5bab04a6369824c84
FabianMauelshagen <f.mauelshagen@live.de>
2020-02-26 12:49:38 +0100

    26.2.20 final version

--- app/src/main/java/com/example/hitchhiker/MainActivity.java
+++ app/src/main/java/com/example/hitchhiker/MainActivity.java
@@ -1,155 +1,171 @@
 package com.example.hitchhiker;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Dialog;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.Drawable;
-import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.view.Gravity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+import static com.example.hitchhiker.MapActivity.MY_PERMISSIONS_REQUEST_READ_CONTACTS;
 
-    GoogleMap mGoogleMap;
-    String profileType = "Driver";
-    GoogleApiClient mGoogleApiClient;
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+    private TextView userRegistration;
+    public static FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (googleServiceAvailable()) {
-            //Toast.makeText(this, "Perfect!", Toast.LENGTH_LONG).show();
-            setContentView(R.layout.activity_main);
-            initMap();
-        } else {
-            // No Google Maps Layout
+        setContentView(R.layout.activity_main);
+        reqPermissions();
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        //Info = (TextView)findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+        userRegistration = (TextView) findViewById(R.id.tvRegister);
+        //Info.setText("Versuche übrig: 5");
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+
+
+        if (user != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, MapActivity.class));
         }
-    }
 
-    private void initMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
-        mapFragment.getMapAsync(this);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideKeyboard(MainActivity.this);
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
 
-    }
-
-    public boolean googleServiceAvailable() {
-        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
-        int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if (isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        } else if (api.isUserResolvableError(isAvailable)) {
-            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
-        }
-        return false;
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+                finish();
+            }
+        });
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        //Toast.makeText(this, "On Map ready!", Toast.LENGTH_LONG).show();
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-        mGoogleApiClient.connect();
+    public void onBackPressed() {
+        moveTaskToBack(true);
     }
 
-    private void goToLocationZoom(double lat, double lng, int zoom) {
-        LatLng ll = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
-        mGoogleMap.moveCamera(update);
-    }
-
-    public void changeProfile(View view) {
-        Button carPerson = findViewById(R.id.profileTypeBtn);
-        TextView text = findViewById(R.id.driverText);
-        if(profileType == "Driver"){
-            profileType = "Hitchhiker";
-            carPerson.setBackground(getResources().getDrawable(R.drawable.person));
-        } else if (profileType == "Hitchhiker"){
-            profileType = "Driver";
-            carPerson.setBackground(getResources().getDrawable(R.drawable.car2));
-        } else {
-            // Nothing
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+        if (firebaseAuth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(MainActivity.this, MapActivity.class));
         }
-        text.setText(profileType);
+    }*/
+
+    private void validate(String userName, String userPassword){
+        userName = userName + "@moco.de";
+        if(userName.isEmpty() || userPassword.isEmpty()){
+            Toast.makeText(MainActivity.this, getString(R.string.logindatenEingeben), Toast.LENGTH_SHORT).show();
+        } else {
+            progressDialog.setMessage(getString(R.string.loginDone));
+            progressDialog.show();
+            firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        progressDialog.dismiss();
+                        Toast.makeText(MainActivity.this, getString(R.string.loginDone), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, MapActivity.class));
+                    } else {
+                        progressDialog.dismiss();
+                        Toast toast = Toast.makeText(MainActivity.this, getString(R.string.loginNotDone), Toast.LENGTH_SHORT);
+                        TextView v = (TextView) toast.getView().findViewById(android.R.id.message);
+                        if( v != null) v.setGravity(Gravity.CENTER);
+                        toast.show();
+                    }
+                }
+            });
+        }
     }
 
-    LocationRequest mLocationRequest;
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        mLocationRequest = LocationRequest.create();
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        mLocationRequest.setInterval(1000);
-
+    public void reqPermissions(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                // public void requestPermissions(@NonNull String[] permissions, int requestCode)
-                // here to request the missing permissions, and then overriding
-                // public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                // int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for Activity#requestPermissions for more details.
-                //
-
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                //Toast.makeText(this, "Permission already granted.", Toast.LENGTH_LONG).show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
                 return;
             }
         }
-        Toast.makeText(this, "Perfect!", Toast.LENGTH_LONG).show();
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    finish();
+                }
+                return;
+            }
 
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if(location == null){
-            Toast.makeText(this, "Cant get current location", Toast.LENGTH_LONG).show();
-        } else {
-            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
-            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 15);
-            mGoogleMap.animateCamera(update);
+            // other 'case' lines to check for other
+            // permissions this app might request.
         }
     }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
 }
